19a96d8138400cd315138c21295ba58644a1ad73
Ola Wingbrant <ola.wingbrant@scania.com>
2026-01-13 13:17:39 +0000

    Add oracles for multiple_contracts.c

---
+++ tests/basic/oracle/multiple_contracts.out.c
@@ -0,0 +1,30 @@
+/* run.config
+   LOG: @PTEST_NAME@.out.c
+   LOG: saida_harness_@PTEST_NAME@.c
+   OPT: -lib-entry -saida -saida-tricera-opts="-acsl" -saida-keep-tmp -saida-out=@PTEST_NAME@.out.c
+*/
+/*
+  This test makes sure that correct contracts are considered and/or discarded.
+*/
+int g;
+
+/*@
+  requires -500 <= x <= 500;
+  ensures \result == x+1;
+*/
+/*@
+  requires g == x && 101 >= x && x >= 0;
+  ensures \old(x) - \result == -1 && g - \result == -1 && \old(g) - \result == -1 && 102 >= \result && \result >= 1;
+*/
+int add_one(int x) {
+  return x+1;
+}
+
+/*@
+  requires 100 >= g >= 0;
+  ensures g == \old(g)+2;
+*/
+void main() {
+  g = add_one(g);
+  g = add_one(g);
+}
---
+++ tests/basic/oracle/saida_harness_multiple_contracts.c
@@ -0,0 +1,34 @@
+/* run.config
+   LOG: @PTEST_NAME@.out.c
+   LOG: saida_harness_@PTEST_NAME@.c
+   OPT: -lib-entry -saida -saida-tricera-opts="-acsl" -saida-keep-tmp -saida-out=@PTEST_NAME@.out.c
+*/
+/*
+  This test makes sure that correct contracts are considered and/or discarded.
+*/
+int g;
+
+
+/*@contract@*/
+int add_one(int x) {
+  return x+1;
+}
+
+
+void main2() {
+  g = add_one(g);
+  g = add_one(g);
+}
+void main()
+{
+
+
+  //The requires-clauses translated into assumes
+  assume(100 >= g && g >= 0);
+
+  //Function call that the harness function verifies
+  main2();
+
+  //The ensures-clauses translated into asserts
+  assert(g == $at("Old", (int)(g)) + 2);
+}
